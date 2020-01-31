d62df1310d1f2fa1512c46ba6f0b76af36963191
HafizurRahman111 <rahman.hafizur@northsouth.edu>
2020-01-31 21:32:22 +0600

    All Files Added

---
+++ Coding and Files/includes/adminDashboardMenu.php
@@ -0,0 +1,38 @@
+<div style="background-color:black;right:0px;width:100px;height:820px;position:fixed;">
+	<a href="allUsers.php">
+		<i class="fas fa-users" style="font-size: 48px; color:white; position:relative; margin-top:18px; margin-left:18px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">All Users</p>
+	</div>
+	<a href="addProduct.php">
+		<i class="fas fa-folder-plus" style="font-size: 48px; color:white; position:relative; margin-top:18px; margin-left: 18px; margin-bottom: 8px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">Add Product</p>
+	</div>
+	<a href="removeProduct.php">
+		<i class="fas fa-folder-minus" style="font-size: 48px; color:white; position:relative; margin-top:18px; margin-left:18px; margin-bottom: 8px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">Remove Product</p>
+	</div>
+	<a href="editProduct.php">
+		<i class="fas fa-folder-open" style="font-size: 48px; color:white; position:relative; margin-top:25px; margin-left:18px; margin-bottom: 8px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">Edit Product</p>
+	</div>
+	<a href="inbox.php">
+		<i class="fas fa-comment" style="font-size: 48px; color:white; position:relative; margin-top:18px; margin-left:20px; margin-bottom: 8px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">Messages</p>
+	</div>
+	<a href="changePassword.php">
+		<i class="fas fa-lock-open" style="font-size: 48px; color:white; position:relative; margin-top:10px; margin-left:18px;  margin-bottom: 12px;"></i>
+	</a> 
+	<div style="background: black; background-size: 100%;margin-top:-10px;width:100px;height:20px;position:relative;">
+		<p align="center">Change Password</p>
+	</div>
+</div>
